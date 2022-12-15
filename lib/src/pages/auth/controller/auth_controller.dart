315d5e0ace103a7154e24353169d15c7edf0b25c
Luciano de Almeida <lucianoal0511@gmail.com>
2022-12-15 11:06:52 -0300

    Finalizado o processo de login

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,12 +1,17 @@
 import 'package:get/get.dart';
-import 'package:greengrocer/src/config/app_data.dart';
+import 'package:greengrocer/src/models/user_model.dart';
 import 'package:greengrocer/src/pages/auth/repository/auth_repository.dart';
 import 'package:greengrocer/src/pages/auth/result/auth_result.dart';
+import 'package:greengrocer/src/pages_routes/app_pages.dart';
+import 'package:greengrocer/src/services/utils_services.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final authRepository = AuthRepository();
+  final utilsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> signIn({
     required String email,
@@ -15,16 +20,23 @@ class AuthController extends GetxController {
     isLoading.value = true;
 
     // await Future.delayed(const Duration(seconds: 2));
-    AuthResult result = await authRepository.signIn(email: email, password: password);
+    AuthResult result =
+        await authRepository.signIn(email: email, password: password);
 
     isLoading.value = false;
 
     result.when(
-      sucess: (user){
-        print(user);
+      sucess: (user) {
+        this.user = user;
+
+        //Get para para passar para a tela de dentro do app
+        Get.offAllNamed(PagesRoutes.baseRoute);
       },
-      error: (message){
-        print(message);
+      error: (message) {
+        utilsServices.showToast(
+          message: message,
+          isError: true,
+        );
       },
     );
   }
