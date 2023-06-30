eef18301f2ff8c2a0ebbf41b45a60eed5468d85f
Luciano de Almeida <lucianoal0511@gmail.com>
2023-06-30 00:50:13 -0300

    Criando controller individual.

--- lib/src/pages/orders/components/order_tile.dart
+++ lib/src/pages/orders/components/order_tile.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:greengrocer/src/models/cart_item_model.dart';
 import 'package:greengrocer/src/models/order_model.dart';
 import 'package:greengrocer/src/pages/common_widgets/payment_dialog.dart';
 import 'package:greengrocer/src/pages/orders/components/order_status_widget.dart';
+import 'package:greengrocer/src/pages/orders/controller/order_controller.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
 
 class OrderTile extends StatelessWidget {
@@ -21,101 +23,111 @@ class OrderTile extends StatelessWidget {
         data: Theme.of(context).copyWith(
             dividerColor:
                 Colors.transparent), //Para sumir com a borda do ExpansionTile
-        child: ExpansionTile(
-          initiallyExpanded: order.status == 'pending_payment',
-          //colapsar várias informacoes em uma coluna
-          title: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text('Pedido: ${order.id}'),
-              Text(
-                utilsServices.formatDateTime(order.createDateTime!),
-                style: const TextStyle(
-                  fontSize: 12,
-                  color: Colors.black,
-                ),
-              ),
-            ],
-          ),
-          childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-          expandedCrossAxisAlignment: CrossAxisAlignment.stretch,//Coloca o conteúdo iniciando da esquerda
-          children: [
-            IntrinsicHeight(
-              //Altura intrinseca, independente
-              child: Row(
+        child: GetBuilder<OrderController>(
+          init: OrderController(),
+          global: false,
+          builder: (controller) {
+            return ExpansionTile(
+              // initiallyExpanded: order.status == 'pending_payment',
+              //colapsar várias informacoes em uma coluna
+              title: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  //Lista de Produtos
-                  Expanded(
-                      flex: 3,
-                      child: SizedBox(
-                        height: 150,
-                        child: ListView(
-                          children: order.items.map((orderItem) {
-                            return _OrderItemWidget(
-                              utilsServices: utilsServices,
-                              orderItem: orderItem,
-                            );
-                          }).toList(),
-                        ),
-                      )),
-                  //Divisão
-                  VerticalDivider(
-                    color: Colors.grey.shade300,
-                    thickness: 2,
-                    width: 8,
-                  ),
-                  //Status do pedido
-                  Expanded(
-                    flex: 2,
-                    child: OrderStatusWidget(
-                      status: order.status,
-                      isOverdue: order.overdueDateTime.isBefore(DateTime
-                          .now()), //Aqui vai fazer a verificação de validade do tempo
+                  Text('Pedido: ${order.id}'),
+                  Text(
+                    utilsServices.formatDateTime(order.createDateTime!),
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
                     ),
                   ),
                 ],
               ),
-            ),
-            // Total
-            Text.rich(
-              TextSpan(
-                style: const TextStyle(
-                  fontSize: 20,
-                ),
-                children: [
-                  const TextSpan(
-                      text: 'Total ',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(
-                    text: utilsServices.priceToCurrency(order.total)
-                  )
-                ],
-              ),
-            ),
-            //Botão Pagamento
-            Visibility(
-              visible: order.status == 'pending_payment',
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
+              childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              expandedCrossAxisAlignment: CrossAxisAlignment.stretch,
+              //Coloca o conteúdo iniciando da esquerda
+              children: [
+                IntrinsicHeight(
+                  //Altura intrinseca, independente
+                  child: Row(
+                    children: [
+                      //Lista de Produtos
+                      Expanded(
+                          flex: 3,
+                          child: SizedBox(
+                            height: 150,
+                            child: ListView(
+                              children: order.items.map((orderItem) {
+                                return _OrderItemWidget(
+                                  utilsServices: utilsServices,
+                                  orderItem: orderItem,
+                                );
+                              }).toList(),
+                            ),
+                          )),
+                      //Divisão
+                      VerticalDivider(
+                        color: Colors.grey.shade300,
+                        thickness: 2,
+                        width: 8,
+                      ),
+                      //Status do pedido
+                      Expanded(
+                        flex: 2,
+                        child: OrderStatusWidget(
+                          status: order.status,
+                          isOverdue: order.overdueDateTime.isBefore(DateTime
+                              .now()), //Aqui vai fazer a verificação de validade do tempo
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                onPressed: (){
-                  showDialog(
-                    context: context,
-                    builder: (_){
-                      return PaymentDialog(order: order,);
+                // Total
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(
+                      fontSize: 20,
+                    ),
+                    children: [
+                      const TextSpan(
+                          text: 'Total ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      TextSpan(text: utilsServices.priceToCurrency(order.total))
+                    ],
+                  ),
+                ),
+                //Botão Pagamento
+                Visibility(
+                  visible: order.status == 'pending_payment',
+                  child: ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return PaymentDialog(
+                            order: order,
+                          );
+                        },
+                      );
                     },
-                  );
-                },
-                icon: Image.asset('assets/app_images/pix.png', height: 18,),
-                label: const Text('Ver QR Code Pix'),
-              ),
-              // replacement: outro widget,//Caso queira incrementar colocando um outro widget no lugar do botão
-            ),
-          ],
+                    icon: Image.asset(
+                      'assets/app_images/pix.png',
+                      height: 18,
+                    ),
+                    label: const Text('Ver QR Code Pix'),
+                  ),
+                  // replacement: outro widget,//Caso queira incrementar colocando um outro widget no lugar do botão
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
